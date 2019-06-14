28fddfe5bb21a79ffa4cf65a600f6e836686e09d
Morttal <51647493+Morttal@users.noreply.github.com>
2019-06-14 08:23:13 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Morttal ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$bc |DF_Team`,"http://twitch.tv/Death Shop")
+client.user.setGame(`Cazanova Forvhh.`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
